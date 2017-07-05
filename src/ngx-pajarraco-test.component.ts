b6abee911a4ee0f714d4954f5cf383a2574d6244
Ernesto La Fontaine <mail@pajarraco.com>
2017-07-05 21:43:01 +1000

    initial commit

---
+++ src/ngx-pajarraco-test.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-pajarraco-test',
+  template: `
+    <h1>Conponent working</h1>
+  `
+})
+
+export class NgxPajarracoTestComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
